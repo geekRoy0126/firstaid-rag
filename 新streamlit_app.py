ccc1b32bc6f3662bb7da6d272b080aa6d42e0b09
geekRoy0126 <ruoyu483@gmail.com>
2025-11-25 17:05:07 -0800

    Update 新streamlit_app.py

--- 新streamlit_app.py
+++ 新streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import json
 import time
 
-API_URL = https://deals-forming-folk-valium.trycloudflare.com/ask"
+API_URL = 'https://gardening-sie-canyon-moving.trycloudflare.com /ask'
 
 st.set_page_config(page_title="First Aid RAG Assistant", page_icon="🚑", layout="centered")
 
@@ -113,3 +113,4 @@ else:
                 unsafe_allow_html=True,
 
             )
+
